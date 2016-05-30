5c448495bbe3f0f33e5a2bd840058bb6492200b2
mulyadi <maxine8ters@gmail.com>
2016-05-30 16:55:15 +0700

    delete  API

--- stockpoint/api/payment.php
+++
@@ -1,9 +0,0 @@
-<?php
-function get($param){
-	_get('konfirmasipembayaran/konfirmasipembayaran');
-$data=mage:getModel('konfirmasipembayaran/konfirmasipembayaran');
-	var_dump($data->getData());
-}
-
-
-// Module konfirmasi pembayaran
